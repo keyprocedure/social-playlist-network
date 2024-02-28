9cbc0c623daaa532110c4a9bdb1e5df344329921
copbrick <anthonyjbenjamin@gmail.com>
2024-02-28 13:53:16 -0800

    add /about route and add temp home page for /

--- application/index.js
+++ application/index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const signale = require("signale");
 const path = require("path");
 const app = express();
+const api = require("./server/api.js");
 const port = 3000;
 
 // Setup Middleware
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/api", api);
 
 // Home Page Route
+app.get("/about", (req, res) => {
+	res.sendFile("public/home.html", { root: __dirname });
+});
 
 app.get("/", (req, res) => {
-	res.sendFile("public/home.html", { root: __dirname });
+	res.send("Home Page");
 });
 
 app.listen(port, () => {
