cba527e043c876cec7529c2c29029c8579de1a11
copbrick <anthonyjbenjamin@gmail.com>
2024-02-28 16:22:25 -0800

    add temp home page to link to about page

--- application/beatlink/src/app/page.js
+++ application/beatlink/src/app/page.js
@@ -1,34 +1,14 @@
+import React from "react";
 import Image from "next/image";
-import "../../public/css/about.css";
 
 export default function Home() {
   return (
-    <div>
-      <div className="title">
-        <h1>Milestone 0</h1>
-        <h2>CSC-648 01 Software Engineering</h2>
-        <h3>Team 03</h3>
-      </div>
-      <div className="container">
-        <a href="about/anthony">
-          <button>Anthony B.</button>
-        </a>
-        <a href="about/warisha">
-          <button>Warisha V.</button>
-        </a>
-        <a href="about/anna">
-          <button>Anna F.</button>
-        </a>
-        <a href="about/guillermo">
-          <button>Guillermo A.</button>
-        </a>
-        <a href="about/zuby">
-          <button>Zuby A.</button>
-        </a>
-        <a href="about/yash">
-          <button>Yash B.</button>
-        </a>
-      </div>
-    </div>
+    <>
+      <h1>Home Page</h1>
+      <a href="/about">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="/about">About Page</button>
+      </a>
+    </>
+
   );
 }
