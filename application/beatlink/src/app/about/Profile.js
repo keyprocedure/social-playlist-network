b70fd416751f535d15c62818d47d29c6834a3fef
anthony <41932917+copbrick@users.noreply.github.com>
2024-03-04 16:22:37 -0800

    add back button on Profile component to go back to /about instead of /

--- application/beatlink/src/app/about/Profile.js
+++ application/beatlink/src/app/about/Profile.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import Image from 'next/image';
-import "../../../public/css/zuby.css"
+import React from "react";
+import Image from "next/image";
+import "../../../public/css/zuby.css";
 
 const Profile = ({ name, image, description }) => {
-    return (
-        <>
-            <a href="/">
-                <button>&larr; Back</button>
-            </a>
-            <h1>{name}</h1>
-            <h2>
-                <Image src={image} alt={name} width={200} height={200} className='zuby_image' />
-            </h2>
-            <p>
-                {description}
-            </p>
-        </>
-    );
+	return (
+		<>
+			<a href="/about">
+				<button>&larr; Back</button>
+			</a>
+			<h1>{name}</h1>
+			<h2>
+				<Image
+					src={image}
+					alt={name}
+					width={200}
+					height={200}
+					className="zuby_image"
+				/>
+			</h2>
+			<p>{description}</p>
+		</>
+	);
 };
 
 export default Profile;
