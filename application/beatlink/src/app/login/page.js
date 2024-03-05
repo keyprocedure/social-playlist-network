847ca103964b69a4c2e5f1f80d92a65545ee3cb4
VZZVWE0 <afelipe@sfsu.edu>
2024-03-04 18:28:35 -0800

    Added page margins

--- application/beatlink/src/app/login/page.js
+++ application/beatlink/src/app/login/page.js
@@ -6,12 +6,14 @@ export default function Login() {
 
   return (
     <>
-    <Navbar/>
-    <h1>This is the login</h1>
-    <button>Login with Spotify</button>
-    </> 
-
-  )
+      <Navbar />
+      <div style={{ margin: "20px", backgroundColor: "rgba(255, 255, 255, .75)", padding: "20px" }}>
+        <h1>This is the login page</h1>
+        <button>Login</button>
+        {/* Add your other content here */}
+      </div>
+    </>
+  );
   
 
   
--- application/beatlink/src/app/page.js
+++ application/beatlink/src/app/page.js
@@ -2,15 +2,18 @@ import React from "react";
 import Image from "next/image";
 import Navbar from "./components/navbar";
 
+
+
 export default function Home() {
 
   return (
     <>
-    <Navbar/>
-    <h1>This is the homepage</h1>
-
-    </> 
-
-  )
+      <Navbar />
+      <div style={{ margin: "20px", backgroundColor: "rgba(255, 255, 255, 0.75)", padding: "20px" }}>
+        <h1>This is the homepage</h1>
+        {/* Add your other content here */}
+      </div>
+    </>
+  );
  
 }
