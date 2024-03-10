b61de98339749ce4cb8345b377973ef73866e4ab
copbrick <anthonyjbenjamin@gmail.com>
2024-03-10 16:13:45 -0700

    add DB integration

--- application/beatlink/src/app/api/playlistinfo/route.js
+++ application/beatlink/src/app/api/playlistinfo/route.js
@@ -1,31 +1,35 @@
 import signale from "signale";
 import { buildPlaylistObject } from "../../../../helpers/spotifyHelper";
+import { createPlaylist } from "../../../../helpers/database/controllers/playlistController";
 
 export const dynamic = "force-dynamic";
 export async function POST(request) {
-	try {
-		const body = await parseJSON(request);
+    try {
+        const body = await parseJSON(request);
 
-		if (!body) {
-			throw new Error("No Body Provided");
-		}
+        if (!body) {
+            throw new Error("No Body Provided");
+        }
 
-		const { playlistURL } = body;
+        const { playlistURL } = body;
 
-		const playlistObject = await buildPlaylistObject(playlistURL);
+        const playlistObject = await buildPlaylistObject(playlistURL);
 
-		return Response.json(playlistObject);
-	} catch (e) {
-		return Response.json({ error: e.message }, { status: 500 });
-	}
+        // Add playlist to DB
+        await createPlaylist(playlistObject);
+
+        return Response.json(playlistObject);
+    } catch (e) {
+        return Response.json({ error: e.message }, { status: 500 });
+    }
 }
 
 async function parseJSON(request) {
-	const contentType = request.headers.get("content-type");
-	if (contentType && contentType.includes("application/json")) {
-		const body = await request.json();
-		return body;
-	} else {
-		return null;
-	}
+    const contentType = request.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+        const body = await request.json();
+        return body;
+    } else {
+        return null;
+    }
 }
