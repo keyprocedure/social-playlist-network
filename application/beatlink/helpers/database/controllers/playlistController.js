fd9b355cd5b5bdeab18f1a1c6d98d0bfe1945e83
anthony <41932917+copbrick@users.noreply.github.com>
2024-03-11 18:23:28 -0700

    latest

--- application/beatlink/helpers/database/controllers/playlistController.js
+++ application/beatlink/helpers/database/controllers/playlistController.js
@@ -3,22 +3,21 @@ import signale from "signale";
 import { connect, disconnect } from "../database";
 
 export const createPlaylist = async (playlistObject) => {
-    try {
-        await connect();
-        const newPlaylist = new Playlist({
-            name: playlistObject.name,
-            description: playlistObject.description,
-            author: playlistObject.author,
-            songs: playlistObject.songs,
-        });
-        await newPlaylist.save();
-        signale.success("Playlist Created");
+	try {
+		await connect();
+		const newPlaylist = new Playlist({
+			name: playlistObject.name,
+			description: playlistObject.description,
+			author: playlistObject.author,
+			songs: playlistObject.songs,
+		});
+		await newPlaylist.save();
+		signale.success("Playlist Created");
 
-        await disconnect();
-    } catch (error) {
-        throw error;
-    }
-    
+		await disconnect();
+	} catch (error) {
+		throw error;
+	}
 };
 
-export const getPlaylist = async
+// export const getPlaylist = async 
