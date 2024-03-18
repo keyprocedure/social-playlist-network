e9c6ca82e78c3a7239086cbefe88aa683b64099d
copbrick <anthonyjbenjamin@gmail.com>
2024-03-18 01:25:34 -0700

    fix incorrect import

--- application/beatlink/helpers/database/controllers/UserController.js
+++ application/beatlink/helpers/database/controllers/UserController.js
@@ -1,13 +1,13 @@
 import User from "../models/User"; // Adjust the path as necessary
 import signale from "signale";
-import { connect, disconnect } from "../database";
+import { connect, disconnect } from "../database.js";
 
 // Assuming this is in a context where createUser makes sense
 export const createUser = async (userObject) => {
     try {
         await connect();
         const newUser = new User({
-            email:userObject.email,
+            email: userObject.email,
             username: userObject.username,
             password: userObject.password,
             birthday: userObject.birthday,
