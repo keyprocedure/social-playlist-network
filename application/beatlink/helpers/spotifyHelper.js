ade45fcaac31ba19cad15f2f6f2be262f17007d3
copbrick <anthonyjbenjamin@gmail.com>
2024-03-20 14:56:15 -0700

    add pagination support, so we can add more than 100 songs which it was previously limited to

--- application/beatlink/helpers/spotifyHelper.js
+++ application/beatlink/helpers/spotifyHelper.js
@@ -52,9 +52,11 @@ async function getPlaylistID(playlistURL) {
 	}
 }
 
-async function importSpotifyPlaylist(playlistURL) {
+async function getPlaylistInformation(playlistURL) {
 	try {
 		accessToken = await getAccessToken();
+
+		signale.info("Getting Playlist Information...");
 		const playlistID = await getPlaylistID(playlistURL);
 
 		const response = await axios.get(
@@ -65,12 +67,83 @@ async function importSpotifyPlaylist(playlistURL) {
 				},
 			}
 		);
-		return response.data; // API Response data to pass to handler so we can build the object to be returned to API
+		return {
+			id: response.data.id,
+			name: response.data.name,
+			description: response.data.description,
+			author: response.data.owner.display_name,
+		}; // API Response data to pass to handler so we can build the object to be returned to API
+	} catch (error) {
+		signale.error(error);
+		throw error;
+	}
+
+}
+
+async function getPlaylistItems(playlistURL) {
+	try {
+		accessToken = await getAccessToken();
+		signale.info("Getting Playlist Items...");
+
+		const playlistID = await getPlaylistID(playlistURL);
+
+		const playlistItems = [];
+
+		// let offset = 0;
+		let query = `https://api.spotify.com/v1/playlists/` + playlistID + `/tracks?offset=0&limit=100`;
+		let response;
+		do {
+			response = await axios.get(query, {
+				headers: {
+					Authorization: "Bearer " + accessToken,
+				},
+			});
+			playlistItems.push(response.data.items);
+			query = response.data.next;
+		} while (response.data.next);
+
+		return playlistItems;
+		// signale.info(response.data.tracks);
+		// while (response.data.tracks.next) {
+		// 	response = await axios.get(
+		// 		query,
+		// 		{
+		// 			headers: {
+		// 				Authorization: "Bearer " + accessToken,
+		// 			},
+		// 		}
+		// 	);
+
+		// 	if (response.data.tracks.next) {
+		// 		query = response.data.tracks.next;
+		// 	}
+		// 	playlistItems.push(response.data.tracks);
+		// }
+
+		// return playlistItems;
+		// while (response.tracks.next) {
+		// 	response = await axios.get(
+		// 		query,
+		// 		{
+		// 			headers: {
+		// 				Authorization: "Bearer " + accessToken,
+		// 			},
+		// 		}
+		// 	);
+
+		// 	query = response.tracks.next;
+		// 	playlistItems.push(response.data.tracks);
+		// }
+
+
+		// return playlistItems; // API Response data to pass to handler so we can build the object to be returned to API
 	} catch (error) {
 		signale.error(error);
 		throw error;
 	}
 }
+
+await buildPlaylistObject("https://open.spotify.com/playlist/37i9dQZF1DWWQRwui0ExPn?si=e810e5b153204bfc");
 
 async function getAuthorGenres(authorID) {
 	try {
@@ -91,42 +164,91 @@ async function getAuthorGenres(authorID) {
 
 export async function buildPlaylistObject(playlistURL) {
 	try {
-		const spotifyPlaylistResponse = await importSpotifyPlaylist(playlistURL);
+		const playlistInformation = await getPlaylistInformation(playlistURL);
+		const playlistItems = await getPlaylistItems(playlistURL);
 		const playlistObject = {};
 
-		const playlistName = spotifyPlaylistResponse.name;
-		const playlistDescription = spotifyPlaylistResponse.description;
-		const playlistAuthor = spotifyPlaylistResponse.owner.display_name;
-
-		playlistObject["name"] = playlistName;
-		playlistObject["description"] = playlistDescription;
-		playlistObject["author"] = playlistAuthor;
-
-		const playlistItems = spotifyPlaylistResponse.tracks.items;
+		playlistObject["name"] = playlistInformation.name;
+		playlistObject["description"] = playlistInformation.description;
+		playlistObject["author"] = playlistInformation.author;
 
 		const songs = [];
 
 		for (const playlistItem of playlistItems) {
-			const song = {};
-			const trackName = playlistItem.track.name;
-			const artistName = playlistItem.track.artists[0].name;
-			const albumName = playlistItem.track.album.name;
-			const duration = playlistItem.track.duration_ms;
-			// const genres = await getAuthorGenres(playlistItem.track.artists[0].id);
-			song["title"] = trackName;
-			song["artist"] = artistName;
-			song["album"] = albumName;
-			song["duration"] = duration;
-			// song["genre"] = genres;
 
-			songs.push(song);
+			for (const item of playlistItem) {
+				const song = {};
+				const trackName = item.track.name;
+				const artistName = item.track.artists[0].name;
+				const albumName = item.track.album.name;
+				const duration = item.track.duration_ms;
+				// const genres = await getAuthorGenres(playlistItem.track.artists[0].id);
+				song["title"] = trackName;
+				song["artist"] = artistName;
+				song["album"] = albumName;
+				song["duration"] = duration;
+				// song["genre"] = genres;
+
+				songs.push(song);
+			}
+			// const song = {};
+			// const trackName = playlistItem.name;
+			// const artistName = playlistItem.artists[0].name;
+			// const albumName = playlistItem.album.name;
+			// const duration = playlistItem.duration_ms;
+			// // const genres = await getAuthorGenres(playlistItem.track.artists[0].id);
+			// song["title"] = trackName;
+			// song["artist"] = artistName;
+			// song["album"] = albumName;
+			// song["duration"] = duration;
+			// // song["genre"] = genres;
+
+			// songs.push(song);
 		}
 
 		playlistObject["songs"] = songs;
 
 		return playlistObject;
 	} catch (error) {
-		signale.error(error);
 		throw error;
 	}
+	// try {
+	// 	const spotifyPlaylistResponse = await importSpotifyPlaylist(playlistURL);
+	// 	const playlistObject = {};
+
+	// 	const playlistName = spotifyPlaylistResponse.name;
+	// 	const playlistDescription = spotifyPlaylistResponse.description;
+	// 	const playlistAuthor = spotifyPlaylistResponse.owner.display_name;
+
+	// 	playlistObject["name"] = playlistName;
+	// 	playlistObject["description"] = playlistDescription;
+	// 	playlistObject["author"] = playlistAuthor;
+
+	// 	const playlistItems = spotifyPlaylistResponse.tracks.items;
+
+	// 	const songs = [];
+
+	// 	for (const playlistItem of playlistItems) {
+	// 		const song = {};
+	// 		const trackName = playlistItem.track.name;
+	// 		const artistName = playlistItem.track.artists[0].name;
+	// 		const albumName = playlistItem.track.album.name;
+	// 		const duration = playlistItem.track.duration_ms;
+	// 		// const genres = await getAuthorGenres(playlistItem.track.artists[0].id);
+	// 		song["title"] = trackName;
+	// 		song["artist"] = artistName;
+	// 		song["album"] = albumName;
+	// 		song["duration"] = duration;
+	// 		// song["genre"] = genres;
+
+	// 		songs.push(song);
+	// 	}
+
+	// 	playlistObject["songs"] = songs;
+
+	// 	return playlistObject;
+	// } catch (error) {
+	// 	signale.error(error);
+	// 	throw error;
+	// }
 }
