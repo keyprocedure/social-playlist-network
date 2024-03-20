b29178036fb6e985aa906a264369797517dcc7bc
copbrick <anthonyjbenjamin@gmail.com>
2024-03-20 12:17:38 -0700

    create POST API route for making song recommendations

---
+++ application/beatlink/src/app/api/songrecommendation/route.js
@@ -0,0 +1,34 @@
+import makeSongRecommendation from "../../../../helpers/aiSongRecommendation";
+import parseJSON from "../../../../helpers/parseJSON";
+export const dynamic = "force-dynamic";
+export async function POST(request) {
+    try {
+        const body = await parseJSON(request);
+
+        if (!body) {
+            throw new Error("No Body Provided");
+        }
+
+        const { artistList, currentSongs } = body;
+
+        // Returns song recommendations in string form which needs to be parsed
+        const rawSongRecommendationsString = await makeSongRecommendation(artistList, currentSongs);
+
+        const songRecommendations = rawSongRecommendationsString.split("\n").map((song) => {
+            const [songName, artist] = song.split(" by ");
+
+            const cleanedSongName = songName.replace(/\\/g, "").replace(/\"/g, "").replace(/\d+\./g, "").trim();
+            return {
+                artist,
+                songName: cleanedSongName,
+            };
+        });
+
+        return Response.json({
+            recommendations: songRecommendations
+        });
+    } catch (e) {
+        return Response.json({ error: e.message }, { status: 500 });
+    }
+}
+
