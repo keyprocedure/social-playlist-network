0fa29362dc19becdf8449adf2b66c81131859054
copbrick <anthonyjbenjamin@gmail.com>
2024-03-22 12:39:31 -0700

    add remove like API route

--- application/beatlink/helpers/database/controllers/postController.js
+++ application/beatlink/helpers/database/controllers/postController.js
@@ -85,3 +85,20 @@ export const addLike = async (postId) => {
         await disconnect();
     }
 }
+
+export const removeLike = async (postId) => {
+    try {
+        await connect();
+
+        if (!await postExists(postId)) {
+            throw new Error("Post does not exist");
+        }
+
+        await Post.updateOne({ id: postId }, { $inc: { likes: -1 } });
+        signale.success("Like Removed");
+    } catch (error) {
+        throw error;
+    } finally {
+        await disconnect();
+    }
+}
