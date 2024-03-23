b1456f1491ec71aefad8240b629e8d8e4dd2fd05
copbrick <anthonyjbenjamin@gmail.com>
2024-03-23 16:11:37 -0700

    add dynamic API route for getting a playlist by it's ID. /api/getplaylist/[id]

---
+++ application/beatlink/src/app/api/getplaylist/[slug]/route.js
@@ -0,0 +1,17 @@
+import { getPlaylist } from "../../../../../helpers/database/controllers/playlistController";
+
+export const dynamic = "force-dynamic";
+export async function GET(request, { params }) {
+    try {
+        const id = params.slug;
+        const playlist = await getPlaylist(id);
+
+        if (!playlist) {
+            return Response.json({ error: "Playlist not found" }, { status: 400 });
+        }
+
+        return Response.json(playlist);
+    } catch (e) {
+        return Response.json({ error: e.message }, { status: 500 });
+    }
+}
