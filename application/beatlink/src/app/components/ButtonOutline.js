2f800adc52804b19d2367b2373b67b986ae7780a
copbrick <anthonyjbenjamin@gmail.com>
2024-03-25 02:39:03 -0700

    add Bootstrap library via CDN script tag, now can access all classes supported by Bootstrap throughout all of components

---
+++ application/beatlink/src/app/components/ButtonOutline.js
@@ -0,0 +1,6 @@
+import React from "react";
+// import { Button } from "@/components/ui/Button";
+
+export function ButtonOutline() {
+    return <button type="button" className="btn btn-outline-primary">Outline</button>
+}
---
+++ application/beatlink/src/app/test/page.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { ButtonOutline } from "../components/ButtonOutline";
+
+export default function Test() {
+    return <ButtonOutline variant="outline">Outline</ButtonOutline>
+}
