5e1dcc17130297d78c00fdc120a0f5ce7f30a6d1
copbrick <anthonyjbenjamin@gmail.com>
2024-03-25 14:36:35 -0700

    add params prop in order to get dynamic route id passed down to CommentSubmit

--- application/beatlink/src/app/post/[id]/page.js
+++ application/beatlink/src/app/post/[id]/page.js
@@ -6,11 +6,14 @@ import { IconButton } from "../../components/IconButton";
 import { FiHeart } from "react-icons/fi";
 import { FaRegShareSquare } from "react-icons/fa";
 import { PiMagicWand } from "react-icons/pi";
+import { CommentSubmit } from "../../components/CommentSubmit";
+import { SendButton } from "../../components/SendButton";
 
-
-export default function PostPage() {
+export default function PostPage({ params }) {
     return (
         // <CustomButton variant="outline" text={"Follow"}></CustomButton>
-        <IconButton icon={<PiMagicWand />} onClick={() => console.log("clicked")} contextValues={{ style: { height: "30px", width: "30px" } }} />
+        // <IconButton icon={<PiMagicWand />} onClick={() => console.log("clicked")} contextValues={{ style: { height: "30px", width: "30px" } }} />
+        <CommentSubmit params={params} />
+        // <IconButton icon={<SendButton />} />
     );
 }
