493e04d302e58de0a5ea2fc72e469c9ef3a26971
copbrick <anthonyjbenjamin@gmail.com>
2024-03-28 14:31:38 -0700

    fix onClick event not being delivered as a prop, hence, buttons would do nothing on click.

--- application/beatlink/src/app/components/CustomButton.js
+++ application/beatlink/src/app/components/CustomButton.js
@@ -2,6 +2,6 @@ import React from "react";
 // import { Button } from "@/components/ui/Button";
 // import "./css/CustomButton.css";
 
-export function CustomButton({ text }) {
-    return <button type="button" className="btn btn-dark">{text}</button>
+export function CustomButton({ text, onClick }) {
+    return <button type="button" className="btn btn-dark" onClick={onClick}>{text}</button>
 }
