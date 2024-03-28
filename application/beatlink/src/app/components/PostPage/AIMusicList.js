b5e0a408b29947cf6ca70d01bddcbe511dc660b0
copbrick <anthonyjbenjamin@gmail.com>
2024-03-28 14:32:31 -0700

    add AIMusicList component, which fetches our API endpoint and renders recommendations based off of this. Since the request takes time, we've added a loading state to be rendered while data is loading

--- application/beatlink/src/app/components/PostPage/AIMusicList.js
+++ application/beatlink/src/app/components/PostPage/AIMusicList.js
@@ -1,15 +1,54 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from 'react';
+import { Spinner } from 'react-bootstrap';
 
-export async function AIMusicList({ playlistObject }) {
-  const musicResponse = await fetch("/api/songrecommendation", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+export function AIMusicList({ playlist }) {
+
+  const [recommendations, setRecommendations] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  let ignore = false; // Flag to prevent duplicate fetches in dev mode
+
+  useEffect(() => {
+    if (!ignore && loading && recommendations.length === 0) {
+      fetchRecommendations(playlist.id)
+        .then(recommendations => {
+          setRecommendations(recommendations.songRecommendations);
+        })
+        .catch(error => {
+          setError(error);
+        })
+    }
+    setLoading(false);
+
+    return () => {
+      ignore = true;
+    };
+  }, [recommendations]);
+
   return (
     <div>
-      <h1>Music List</h1>
+      {recommendations.length > 0 && !loading ? (
+        <ul>
+          {recommendations.map(song => (
+            <li key={song.id}>
+              {song.songName} by {song.artist}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <Spinner animation="border" role="status" />
+      )}
     </div>
-  );
+  )
+}
+
+async function fetchRecommendations(playlistId) {
+  const response = await fetch(`/api/songrecommendation/${playlistId}`);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch recommendations');
+  }
+
+  return response.json();
 }
