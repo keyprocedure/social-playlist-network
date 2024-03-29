7e01cdc4710eec40134f35be7a1c7ccc436445ef
copbrick <anthonyjbenjamin@gmail.com>
2024-03-29 15:54:36 -0700

    add width/height props

--- application/beatlink/src/app/components/SendButton.js
+++ application/beatlink/src/app/components/SendButton.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export function SendButton() {
+export function SendButton({ width, height }) {
     return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clipPath="url(#clip0_2497_26182)">
                 <path d="M4.99988 12L4.39577 6.56299C4.22284 5.0067 5.82456 3.86433 7.2397 4.53465L19.1841 10.1925C20.7092 10.9149 20.7092 13.0851 19.1841 13.8075L7.23971 19.4653C5.82457 20.1357 4.22284 18.9933 4.39577 17.437L4.99988 12ZM4.99988 12H11.9999" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </g>
