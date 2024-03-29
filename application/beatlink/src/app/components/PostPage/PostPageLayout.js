c8f56671789981c7c3b123d56fe62fb8342dfbce
copbrick <anthonyjbenjamin@gmail.com>
2024-03-29 14:00:31 -0700

    Back Button done

--- application/beatlink/src/app/components/PostPage/PostPageLayout.js
+++ application/beatlink/src/app/components/PostPage/PostPageLayout.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { AIRecommendation } from "./AIRecommendation";
-import ImageBlur from "./ImageBlur";
-
+import PostImage from "./PostImage";
+import ShareButton from "./ShareButton";
+import BackButton from "./BackButton";
 export default function PostPageLayout({ playlist }) {
     return (
         <div>
             {/* <AIRecommendation playlist={playlist} /> */}
-            <ImageBlur playlist={playlist} />
+            {/* <PostImage playlist={playlist} /> */}
+            {/* <ShareButton /> */}
+            <BackButton width={"40px"} height={"40px"} />
         </div>
     );
 }
