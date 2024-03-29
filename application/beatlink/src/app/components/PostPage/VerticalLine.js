824453891a8ef2ef769392a1c752d4e04cb53902
copbrick <anthonyjbenjamin@gmail.com>
2024-03-29 15:50:47 -0700

    add Vertical Line component

---
+++ application/beatlink/src/app/components/PostPage/VerticalLine.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function VerticalLine({ width, height }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 1 368" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <line x1="0.5" x2="0.5" y2="368" stroke="#DEDEDE" />
+        </svg>
+
+    )
+}
