3b5eef864ab3aa824bab41768c55d176aa26f288
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-03 00:01:29 -0700

    add priority true on Image component to preload the image

--- application/beatlink/src/app/components/PostPage/PostImage.js
+++ application/beatlink/src/app/components/PostPage/PostImage.js
@@ -6,57 +6,62 @@ import Modal from "react-bootstrap/Modal";
 import { ListGroup } from "react-bootstrap";
 
 export default function PostImage({ playlist }) {
+	const [isHovered, setIsHovered] = useState(false);
+	const [show, setShow] = useState(false);
+	const handleClose = () => {
+		setShow(false);
+		setIsHovered(false);
+	};
+	const handleShow = () => {
+		setShow(true);
+	};
+	return (
+		<div
+			className="image-container"
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}>
+			<Image
+				className="playlist-img"
+				src={playlist.image}
+				width={400}
+				height={400}
+				alt="description"
+				priority={true}
+			/>
 
-    const [isHovered, setIsHovered] = useState(false);
-    const [show, setShow] = useState(false);
-    const handleClose = () => {
-        setShow(false);
-        setIsHovered(false);
-    }
-    const handleShow = () => {
-        setShow(true);
-    };
-    // return (
-    //     <div className="img-container" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-    //         <Image className="img" src={playlist.image} width={300} height={300} />
-    //         {isHovered && (
-    //             <CustomButton text={"Open Playlist"} />
-    //         )}
-    //     </div>
-    // );
-    return (
-        <div className="image-container" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} >
-            <Image className="playlist-img" src={playlist.image} width={400} height={400} alt="description" />
+			{isHovered && (
+				<div>
+					<CustomButton
+						className="overlay-button btn btn-dark"
+						text={"View Playlist"}
+						onClick={handleShow}
+					/>
+					<Modal show={show} onHide={handleClose}>
+						<Modal.Header closeButton>
+							<Modal.Title>{playlist.name}</Modal.Title>
+						</Modal.Header>
+						<Modal.Body className="modal-body">
+							<p>Here are your songs:</p>
 
-            {isHovered && (
-                <div>
-                    <CustomButton className="overlay-button btn btn-dark" text={"View Playlist"} onClick={handleShow} />
-                    <Modal show={show} onHide={handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>{playlist.name}</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body className="modal-body">
-                            <p>
-                                Here are your songs:
-                            </p>
+							<ListGroup>
+								{playlist.songs.map((song, index) => (
+									<div key={index}>
+										<ListGroup.Item>
+											{song.title} - {song.artist}
+										</ListGroup.Item>
+									</div>
+								))}
+							</ListGroup>
 
-                            <ListGroup>
-                                {playlist.songs.map((song, index) => (
-                                    <div key={index}>
-                                        <ListGroup.Item>{song.title} - {song.artist}</ListGroup.Item>
-                                    </div>
-                                ))}
-                            </ListGroup>
-
-                            {/* <AIMusicList playlist={playlist} /> */}
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <CustomButton text={"Close"} onClick={handleClose} />
-                        </Modal.Footer>
-                    </Modal>
-                </div>
-            )}
-            {/* <button className="overlay-button">Open Playlist</button> */}
-        </div>
-    )
+							{/* <AIMusicList playlist={playlist} /> */}
+						</Modal.Body>
+						<Modal.Footer>
+							<CustomButton text={"Close"} onClick={handleClose} />
+						</Modal.Footer>
+					</Modal>
+				</div>
+			)}
+			{/* <button className="overlay-button">Open Playlist</button> */}
+		</div>
+	);
 }
