506e6c80051bc9a0239bd3bc3f57d91ce0915d64
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-02 23:56:46 -0700

    update formatting

--- application/beatlink/src/app/components/PostPage/BackButton.js
+++ application/beatlink/src/app/components/PostPage/BackButton.js
@@ -18,7 +18,7 @@ function BackButtonIcon({ width, height }) {
         <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clipPath="url(#clip0_6_16)">
                 <path d="M5 12H19" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M12 5L5 12L12 19" stroke="black" strokeWidth="2" stroke-Linecap="round" strokeLinejoin="round" />
+                <path d="M12 5L5 12L12 19" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </g>
             <defs>
                 <clipPath id="clip0_6_16">
