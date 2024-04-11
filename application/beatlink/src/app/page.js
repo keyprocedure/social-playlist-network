43c5bf04ceaeba02ffc16cf7aca11124001914b0
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-10 17:05:22 -0700

    make navbar a global layout, so we don't have to constantly individually import it in every component. It'll always be there now

--- application/beatlink/src/app/page.js
+++ application/beatlink/src/app/page.js
@@ -1,20 +1,22 @@
 "use client";
 import React, { useContext } from "react";
-import Image from "next/image";
-import Navbar from "./components/navbar";
-import PostPage from "./post/[id]/page";
-import checkSessionCookie from '../../helpers/hooks/checkSessionCookie';
+import checkSessionCookie from "../../helpers/hooks/checkSessionCookie";
 
 export default function Home() {
   const isLoading = checkSessionCookie();
 
   return (
     <>
-      <Navbar />
       {isLoading ? (
         <div></div>
       ) : (
-        <div style={{ margin: "20px", backgroundColor: "rgba(255, 255, 255, 0.75)", padding: "20px" }}>
+        <div
+          style={{
+            margin: "20px",
+            backgroundColor: "rgba(255, 255, 255, 0.75)",
+            padding: "20px",
+          }}
+        >
           <h1>Homepage is under construction.</h1>
           {/* Add your other content here */}
         </div>
