1f510f8b33f9f7f8a7f8c44aff657a8c9b0bf838
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-15 11:46:56 -0700

    add redirect if user is not logged in on post page, to the login page. Also, add fetchUser function to get user's information into a user state variable

--- application/beatlink/src/app/components/PostPage/PostPageLayout.js
+++ application/beatlink/src/app/components/PostPage/PostPageLayout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AIRecommendation } from "./AIRecommendation";
 import PostImage from "./PostImage";
 import ShareButton from "./ShareButton";
@@ -8,14 +8,22 @@ import { LikeButton } from "./LikeButton";
 import { CommentSubmit } from "./CommentSubmit";
 import { VerticalLine } from "./VerticalLine";
 import { CustomButton } from "../CustomButton";
-import Navbar from "../navbar";
+import Cookies from "js-cookie";
 import "../css/PostPageLayout.css";
 import { CommentCard } from "./CommentCard";
+import CheckSessionCookie from "../../../../helpers/hooks/CheckSessionCookie";
 
 //FIX: Change user ID with username, add a way to convert the ID to a username
 export default function PostPageLayout({ playlist, post }) {
   const [comments, setComments] = useState(post.comments);
   const [follow, setFollow] = useState("Follow");
+  const [user, setUser] = useState(null);
+
+  const isLoggedIn = CheckSessionCookie();
+
+  useEffect(() => {
+    fetchUser(Cookies.get("userid")).then((user) => setUser(user));
+  }, []);
 
   async function handleFollow() {
     if (follow === "Followed") {
@@ -31,69 +39,97 @@ export default function PostPageLayout({ playlist, post }) {
 
   return (
     <>
-      <div className="page-grid-container">
-        <div className="back-button">
-          <BackButton width={"40px"} height={"40px"} />
-        </div>
-        <div className="posted-by">
-          <ProfileCard
-            primaryText={"Posted by:"}
-            primaryTextColor={"grey"}
-            secondaryText={post.user_id}
-            secondaryTextColor={"grey"}
-          />
-        </div>
-        <div className="post-area">
-          <div className="post-title">
-            {post.postTitle}
-            <CustomButton
-              className={"btn btn-dark follow-btn"}
-              text={follow}
-              onClick={handleFollow}
+      {isLoggedIn ? (
+        <div></div>
+      ) : (
+        <div className="page-grid-container">
+          <div className="back-button">
+            <BackButton width={"40px"} height={"40px"} />
+          </div>
+          <div className="posted-by">
+            <ProfileCard
+              primaryText={"Posted by:"}
+              primaryTextColor={"grey"}
+              secondaryText={post.user_id}
+              secondaryTextColor={"grey"}
             />
           </div>
-          <div className="post-content">
-            <PostImage playlist={playlist} />
-          </div>
-          <div className="post-interactions">
-            <div className="ai-button">
-              <AIRecommendation
-                playlist={playlist}
-                width={"40px"}
-                height={"40px"}
+          <div className="post-area">
+            <div className="post-title">
+              {post.postTitle}
+              <CustomButton
+                className={"btn btn-dark follow-btn"}
+                text={follow}
+                onClick={handleFollow}
               />
             </div>
-            <div className="share-button">
-              <ShareButton width={"30px"} height={"30px"} />
+            <div className="post-content">
+              <PostImage playlist={playlist} />
             </div>
-            <div className="like-button">
-              <LikeButton width={"33px"} height={"33px"} post={post} />
-            </div>
-          </div>
-        </div>
-        <div className="vertical-line">
-          <VerticalLine />
-        </div>
-        <div className="comment-section">
-          {/* add section for comments */}
-          {comments.map((comment, id) => {
-            return (
-              <div key={id}>
-                <CommentCard
-                  username={comment.userId}
-                  comment={comment.comment}
+            <div className="post-interactions">
+              <div className="ai-button">
+                <AIRecommendation
+                  playlist={playlist}
+                  width={"40px"}
+                  height={"40px"}
                 />
               </div>
-            );
-          })}
+              <div className="share-button">
+                <ShareButton width={"30px"} height={"30px"} />
+              </div>
+              <div className="like-button">
+                <LikeButton width={"33px"} height={"33px"} post={post} />
+              </div>
+            </div>
+          </div>
+          <div className="vertical-line">
+            <VerticalLine />
+          </div>
+          <div className="comment-section">
+            {/* add section for comments */}
+            {comments.map((comment, id) => {
+              return (
+                <div key={id}>
+                  <CommentCard
+                    username={comment.userId}
+                    comment={comment.comment}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <div className="comment-submission">
+            <CommentSubmit
+              post={post}
+              handleCommentSubmission={handleCommentSubmission}
+            />
+          </div>
         </div>
-        <div className="comment-submission">
-          <CommentSubmit
-            post={post}
-            handleCommentSubmission={handleCommentSubmission}
-          />
-        </div>
-      </div>
+      )}
     </>
   );
+}
+
+async function fetchUser(userId) {
+  try {
+    const response = await fetch("/api/getuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error);
+    }
+
+    return { success: true, data };
+  } catch (error) {
+    throw { success: false, error: error.message };
+  }
 }
