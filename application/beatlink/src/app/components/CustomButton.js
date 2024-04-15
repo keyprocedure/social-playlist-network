3613ef92175472050283bd59f3ac14f2ca8cc7e8
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-15 00:47:32 -0700

    add type field

--- application/beatlink/src/app/components/CustomButton.js
+++ application/beatlink/src/app/components/CustomButton.js
@@ -2,6 +2,15 @@ import React from "react";
 // import { Button } from "@/components/ui/Button";
 // import "./css/CustomButton.css";
 
-export function CustomButton({ text, onClick, className }) {
-    return <button type="button" className={className || "btn btn-dark"} onClick={onClick}>{text}</button>
+export function CustomButton({ type, text, onClick, className }) {
+  return (
+    <button
+      type={type || "button"}
+      className={className || "btn btn-dark"}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
+
