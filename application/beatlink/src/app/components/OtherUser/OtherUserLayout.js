f5ca729f2ed319205fdc9c9e9661d6ff20bfb503
yash <ybhadiyadra@sfsu.edu>
2024-04-29 18:11:17 -0700

    Resolved

--- application/beatlink/src/app/components/OtherUser/OtherUserLayout.js
+++ application/beatlink/src/app/components/OtherUser/OtherUserLayout.js
@@ -32,7 +32,8 @@ const OtherUserPageLayout = async ({ userData, playlistImages, fetchData, userId
         throw new Error("Follow user failed. Try again");
       }
       fetchData && await fetchData();
-      { isFollowing ? setIsFollowing(false) : setIsFollowing(true) }
+      setIsFollowing(!isFollowing);
+      
       return { success: true };
     } catch (error) {
       console.error("An error occurred during the follow process", error);
