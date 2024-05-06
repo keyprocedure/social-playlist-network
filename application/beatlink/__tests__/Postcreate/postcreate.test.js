ebed8fcc68bb8f902420644460f4864a3274ce89
Guillermo Alcantara <alcantaraguillermo23@gmail.com>
2024-05-05 23:29:46 -0700

    unit test createpost

---
+++ application/beatlink/__tests__/Postcreate/postcreate.test.js
@@ -0,0 +1,56 @@
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import CreatePost from '../../src/app/components/CreatePost';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+}));
+jest.mock('js-cookie');
+
+describe('CreatePost Component', () => {
+  const mockPush = jest.fn();
+  const mockFetch = global.fetch = jest.fn();
+
+  beforeEach(() => {
+    useRouter.mockImplementation(() => ({ push: mockPush }));
+    Cookies.get.mockReturnValue('123'); // Mock user ID
+    mockFetch.mockClear().mockImplementation(() => 
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({
+        success: true,
+        data: { postId: '123456' }
+      })
+    })
+  );
+  });
+
+  it('should make an API call with correct parameters when form is submitted', async () => {
+    const { getByPlaceholderText, getByRole } = render(<CreatePost />);
+    const titleInput = getByPlaceholderText('Post Title');
+    const linkInput = getByPlaceholderText('Spotify Playlist Link');
+    const createButton = getByRole('button', { name: 'Create Post' });
+    
+
+
+    fireEvent.change(titleInput, { target: { value: 'My New Post' } });
+    fireEvent.change(linkInput, { target: { value: 'https://open.spotify.com/playlist/123456' } });
+    fireEvent.click(createButton);
+
+    await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
+    expect(mockFetch).toHaveBeenCalledWith('/api/createpost', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        postTitle: 'My New Post',
+        spotifyLink: 'https://open.spotify.com/playlist/123456',
+        userId: '123',
+        playlistId: '123456',
+      }),
+    });
+    
+  });
+});
--- application/beatlink/__tests__/login-page/login.test.js
+++ application/beatlink/__tests__/login-page/login.test.js
@@ -94,3 +94,4 @@ describe('LoginPageLayout', () => {
         });
     });
 });
+
